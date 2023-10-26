f1897b58fff6347fd945e1ed4070a739645e702f
s22020 <s22020@pjwstk.edu.pl>
2023-10-26 15:43:01 +0200

    Document fuzzy logic antecedents, consequents and rules

--- roomba_vacuum_robot/fuzzy_vacuum.py
+++ roomba_vacuum_robot/fuzzy_vacuum.py
@@ -3,6 +3,46 @@ import numpy as np
 import skfuzzy as fuzz
 from skfuzzy import control as ctrl
 
+
+"""
+* Antecednets (Inputs)
+   - `cleanliness`
+      * Universe (ie, crisp value range): How dirty is a tile on the floor, 
+      on a scale of 0 to 10?
+      * Fuzzy set (ie, fuzzy value range): very dirty, dirty, little dirty, clean
+   - `battery level`
+      * Universe: How much battery has the robot left, on a scale of 0 to 100?
+      * Fuzzy set: low, medium, high
+   - `water level`
+      * Universe: What is the water level, on a scale of 0 to 5?
+      * Fuzzy set: low, medium, high
+* Consequents (Outputs)
+   - `motor speed`
+      * Universe: How fast do motors rotate per minute, on a scale of 0 to 10
+      * Fuzzy set: slow, average, fast
+   - `water dispense?`
+      * Universe: How much water will be disposed, on a scale of 0 to 10
+      * Fuzzy set: low, medium, high
+   - `time per tile`
+      * Universe: How much time does a robot spend on one tile, on a scale of 0 to 3
+      * Fuzzy set: short, average, long
+* Rules
+   - IF the *cleanliness* is very dirty *and* the *battery level* and *water level*
+     is high,
+     THEN the motor_speed will be high and time_per_tile will be long and water disposal will be high.
+   - IF the *cleanliness* is dirty and water level is high, THEN the motor speed and time per tile and water dispense will be medium will be average.
+   - IF the *cleanliness* is a little dirty *or* the *battery level* is medium
+     THEN the motor speed will be low and time per tile will be medium.
+   - IF the *cleanliness* is clean *or* the *battery level* is low
+     THEN the motor speed will be very low and time per tile will be short.
+* Usage
+   - If I tell this controller that I rated:
+      * the cleanliness as 1.5, and
+      * the battery level as 95,
+   - it would recommend I leave:
+      * a x motor speed.
+"""
+
 cleanliness = ctrl.Antecedent(np.arange(0, 11, 1), 'cleanliness')
 #is_next_tile_obstacle = ctrl.Antecedent(np.arange(0, 2, 1), 'is_next_tile_obstacle')
 #water_level = ctrl.Antecedent(np.arange(0, 3, 1), 'water_level')
