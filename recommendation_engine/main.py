8de235aa347cb9827143f14173c470b88652fe3e
s22020 <s22020@pjwstk.edu.pl>
2023-11-15 18:25:15 +0100

    Add movies titles unification through Filmweb API

--- recommendation_engine/main.py
+++ recommendation_engine/main.py
@@ -1,4 +1,5 @@
 import csv
+import requests
 import json
 
 movies_dict = {}
@@ -14,3 +15,22 @@ with open('movie_opinions.csv', newline='', encoding='utf-8-sig') as csv_file:
         # transform to json
         movies_dict[person] = {row[i]: float(row[i+1]) for i in range(0, len(row), 2) if row[i] != ''}
         # print(json.dumps(movies_dict, indent=2))
+
+headers = {"x-locale":"pl_PL", "content-type":"application/json"}
+# load csv
+for key in movies_dict:
+    for movie_title in movies_dict[key]:
+        movie_title = requests.utils.quote(movie_title)
+        req_movie_search = requests.get("https://www.filmweb.pl/api/v1/live/search?query=" 
+                                        + movie_title + "&pageSize=1", headers=headers)
+        resp_movie_search = req_movie_search.json()
+        movie_id = resp_movie_search["searchHits"][0]["id"]
+        print(movie_id)
+        req_movie_info = requests.get('https://www.filmweb.pl/api/v1/title/' + str(movie_id) + '/info',
+                              headers=headers)
+        resp_movie_info = req_movie_info.json()
+        print(resp_movie_info)
+        if "originalTitle" in resp_movie_info:
+            if movie_title != resp_movie_info["originalTitle"]:
+                movies_dict[key] = resp_movie_info["originalTitle"]
+                print(movies_dict[key])
