d0ae1e02e405428c1e096ecf73e37bb896c3be25
Michał Siwek <mjzsiwek@gmail.com>
2023-12-11 22:41:43 +0100

    Added Fashion MNIST dataset neural network model

---
+++ neural_networks/fashion.py
@@ -0,0 +1,28 @@
+import numpy as np
+import tensorflow as tf
+
+from model_factory import ModelFactory
+
+# Dataset creation and standardization
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_data, train_result), (test_data, test_result) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+# Standardization of data, here it is simple because we can simply divide values by 255
+train_data = train_data / 255.0
+test_data = test_data / 255.0
+
+# Model creation, training and evaluation
+model_factory = ModelFactory()
+
+input_layer = tf.keras.layers.Flatten(input_shape = (28, 28))
+
+model = model_factory.create_model(input_layer, [100, 100], 10)
+
+loss_function = tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True)
+
+model_factory.train_model(model, "adam", loss_function, "fashion", train_data, train_result, 10)
+
+model_factory.evaluate_model(model, test_data, test_result)
