bb7c0533e83b97a61ca4d264af252b23d8021f6e
Michał Siwek <mjzsiwek@gmail.com>
2023-12-11 22:41:12 +0100

    Code improvements

--- neural_networks/banknote.py
+++ neural_networks/banknote.py
@@ -1,4 +1,5 @@
 import numpy as np
+import tensorflow as tf
 
 from dataset_creator import DatasetCreator
 from model_factory import ModelFactory
@@ -11,10 +12,15 @@ train_data_std, test_data_std = DatasetCreator.standardize_data(train_data, test
 train_result = np.array(train_result)
 test_result = np.array(test_result)
 
+# Model creation, training and evaluation
 model_factory = ModelFactory()
 
-model = model_factory.create_model([50, 50], (None, 4), 1)
+input_layer = tf.keras.layers.Input(shape = (None, 4))
 
-model_factory.train_model(model, "banknote", train_data_std, train_result, 100)
+model = model_factory.create_model(input_layer, [50, 50], 1)
+
+loss_function = tf.keras.losses.BinaryCrossentropy()
+
+model_factory.train_model(model, "adam", loss_function, "banknote", train_data_std, train_result, 100)
 
 model_factory.evaluate_model(model, test_data_std, test_result)
--- neural_networks/model_factory.py
+++ neural_networks/model_factory.py
@@ -3,8 +3,8 @@ import datetime
 
 
 class ModelFactory:
-    def create_model(self, neuron_amounts, input_shape, outputs):
-        layers = [tf.keras.layers.Input(shape = input_shape)]
+    def create_model(self, input_layer, neuron_amounts, outputs):
+        layers = [input_layer]
 
         for neuron_amount in neuron_amounts:
             layers += [tf.keras.layers.Dense(neuron_amount, activation = 'relu')]
@@ -13,8 +13,8 @@ class ModelFactory:
 
         return tf.keras.Sequential(layers)
 
-    def train_model(self, model, log_name, train_data, train_result, epochs):
-        model.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    def train_model(self, model, optimizer, loss_function, log_name, train_data, train_result, epochs):
+        model.compile(optimizer = optimizer, loss = loss_function, metrics = ['accuracy'])
 
         logdir = "logs/fit/" + log_name + "_" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
 
@@ -23,6 +23,7 @@ class ModelFactory:
         model.fit(train_data, train_result, epochs = epochs, callbacks = [tensorboard_callback])
 
     def evaluate_model(self, model, test_data, test_result):
+        print("\nEvaluating model:")
         loss, accuracy = model.evaluate(test_data, test_result)
 
         print(f"Final loss: {loss} \nFinal accuracy: {accuracy}")
