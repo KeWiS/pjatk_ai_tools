023e31eb11bbaeeeb91b638afea2914e4d9ad83a
Michał Siwek <mjzsiwek@gmail.com>
2023-12-11 22:43:11 +0100

    Comment complement

--- neural_networks/fashion.py
+++ neural_networks/fashion.py
@@ -8,7 +8,8 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 
 (train_data, train_result), (test_data, test_result) = fashion_mnist.load_data()
 
-class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+# Class names for the images, their position in array corresponds to the actual label they have in the dataset
+# class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
 
 # Standardization of data, here it is simple because we can simply divide values by 255
 train_data = train_data / 255.0
